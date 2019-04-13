166a7eee61a49e051a34268fbab45e7e1c4fe785
Gabriel Torres <gtorres043@west-mec.org>
2019-04-12 18:03:44 -0700

    applied form events

--- co_order.js
+++ co_order.js
@@ -35,33 +35,60 @@ window.addEventListener("load", function () {
 
       //calculate the cost of the order
       calcOrder();
+
+      //event handlers for the web form
+      orderForm.elements.model.onchange = calcOrder;
+      orderForm.elements.qty.onchange = calcOrder;
+
+      var planOptions = document.querySelectorAll('input[name="protection"]');
+      for (var i = 0; i < planOptions.length; i++) {
+            planOptions[i].onclick = calcOrder;
+      }
 });
 
 function calcOrder() {
       var orderForm = document.forms.orderForm;
 
       //calculate the initial cost of the order
-      var mIndex = orderForm.elements.model.selctedIndex;
+      var mIndex = orderForm.elements.model.selectedIndex;
       var mCost = orderForm.elements.model.options[mIndex].value;
-      var qIndex = orderForm.elements.qty.selctedIndex;
+      var qIndex = orderForm.elements.qty.selectedIndex;
       var quantity = orderForm.elements.qty[qIndex].value;
 
       //initial cost = model x quantity
       var initialCost = mCost * quantity;
-      orderForm.elements.initialCost.value = initialCost;
+      orderForm.elements.initialCost.value = formatUSACurrency(initialCost);
 
       //retrieve the cost of the users protection plan
-      var pCost = document.querySelector('input [name="protection]:checked').value * quantity;
-      orderForm.elements.protectionCost.value = pCost;
+      var pCost = document.querySelector('input[name="protection"]:checked').value * quantity;
+      orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
 
       //calculate the order subtotal
-      orderform.elements.subtotal.value = initialCost + pCost;
+      orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
 
       //calculate the slaes tax 
       var salesTax = 0.05 * (initialCost + pCost);
-      orderForm.elements.salesTax.value = salesTax;
+      orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
 
       //calculate the cost of the total order 
       var totalCost = initialCost + pCost + salesTax;
-      orderForm.elements.totalCost.value = totalCost;
+      orderForm.elements.totalCost.value = formatUSACurrency(totalCost);
+
+      //store the order details
+      orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
+      orderForm.elements.protectionName.value = document.querySelector('input[name="protection"]:checked').nextSibling.nodeValue;
+}
+
+function formatNumber(val, decimals) {
+      return val.toLocaleString(undefined, {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals
+      });
+}
+
+function formatUSACurrency(val) {
+      return val.toLocaleString('en-US', {
+            style: "currency",
+            currency: "USD"
+      });
 }
--- co_payment.js
+++ co_payment.js
@@ -43,3 +43,8 @@
       Returns true of idNum satisfies the Luhn Algorithm
 
 */
+
+window.addEventListener("load", function () {
+      //retrieve the field value pairs from the url
+      var formData = location.search.slice(1);
+});
